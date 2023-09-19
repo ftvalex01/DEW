1e2ab8d52c755c8dba90107b12c321fd8b08c21f
ftvalex01 <ftvalex@hotmail.com>
2023-09-19 23:24:42 +0100

    arreglando el codigo spageti

--- 13-09-2023/ejercicio2/js/index.js
+++ 13-09-2023/ejercicio2/js/index.js
@@ -1,124 +1,85 @@
+
 let caja = document.getElementById("texto-caja");
-    let texto = document.getElementById("texto");
-    let acierto = document.getElementById("acierto");
-    let fallo = document.getElementById("fallo");
-    let totalAciertos = document.getElementById("total-aciertos");
-    let totalFallos = document.getElementById("total-fallos");
-    let vueltas = 0;
-    let botonesHabilitados = true;
-    let arrayCuestiones = [
-      "¿Es el backend la parte visible de la web?",
-      "¿Es una ventaja de los frameworks el coste? ",
-      "¿Nacieron los scripts como fragmentos de código que realizaban ciertas tareas concretas? ",
-      "¿Es necesaria las etiquetas script para usar codigo javascript?",
-      "¿Es Mongo una tecnologia del front-end?",
-    ];
+let texto = document.getElementById("texto");
+let acierto = document.getElementById("acierto");
+let fallo = document.getElementById("fallo");
+let totalAciertos = document.getElementById("total-aciertos");
+let totalFallos = document.getElementById("total-fallos");
+let preguntaActual = 0;
+let botonesHabilitados = true;
 
-    function desactivarBotones() {
-      acierto.disabled = true;
-      fallo.disabled = true;
-      acierto.style.backgroundColor = "grey";
-      fallo.style.backgroundColor = "grey";
-    }
 
-    function habilitarBotones() {
-      acierto.disabled = false;
-      fallo.disabled = false;
-      acierto.style.backgroundColor = "green";
-      fallo.style.backgroundColor = "red";
-    }
+const arrayPreguntasRespuestas = [
+  { pregunta: "¿Es el backend la parte visible de la web?", respuestaCorrecta: false },
+  { pregunta: "¿Es una ventaja de los frameworks el coste?", respuestaCorrecta: true },
+  { pregunta: "¿Nacieron los scripts como fragmentos de código que realizaban ciertas tareas concretas?", respuestaCorrecta: true },
+  { pregunta: "¿Es necesaria las etiquetas script para usar código JavaScript dentro de un documento HTML?", respuestaCorrecta: true },
+  { pregunta: "¿Es Mongo una tecnología del front-end?", respuestaCorrecta: false },
+];
 
-    function preguntaAcierto(evento) {
-      trivial(evento);
-      return;
-    }
-    function preguntaFallo(evento) {
-      trivial(evento);
-      return;
-    }
 
-    function trivial(evento) {
-      if (texto.innerText == "") {
-        texto.innerText = arrayCuestiones[0];
-      } else if (texto.innerText.match("visible")) {
-        if (evento == false) {
-          totalAciertos.innerHTML++;
-          caja.style.backgroundColor = "green";
-        } else {
-          totalFallos.innerHTML++;
-          caja.style.backgroundColor = "red";
-        }
-        vueltas++;
-        desactivarBotones();
-        setTimeout(function () {
-          habilitarBotones();
-          caja.style.backgroundColor = "";
-          texto.innerText = arrayCuestiones[1];
-        }, 3000);
-      } else if (texto.innerText.match("ventaja")) {
-        if (evento == true) {
-          totalAciertos.innerHTML++;
-          caja.style.backgroundColor = "green";
-        } else {
-          totalFallos.innerHTML++;
-          caja.style.backgroundColor = "red";
-        }
-        vueltas++;
-        desactivarBotones();
-        setTimeout(function () {
-          habilitarBotones();
-          caja.style.backgroundColor = "";
-          texto.innerText = arrayCuestiones[2];
-        }, 3000);
-      } else if (texto.innerText.match("scripts")) {
-        if (evento == true) {
-          totalAciertos.innerHTML++;
-          caja.style.backgroundColor = "green";
-        } else {
-          totalFallos.innerHTML++;
-          caja.style.backgroundColor = "red";
-        }
-        desactivarBotones();
-        vueltas++;
-        setTimeout(function () {
-          habilitarBotones();
-          caja.style.backgroundColor = "";
-          texto.innerText = arrayCuestiones[3];
-        }, 3000);
-      } else if (texto.innerText.match("etiquetas")) {
-        if (evento == true) {
-          totalAciertos.innerHTML++;
-          caja.style.backgroundColor = "green";
-        } else {
-          totalFallos.innerHTML++;
-          caja.style.backgroundColor = "red";
-        }
-        vueltas++;
-        desactivarBotones();
-        setTimeout(function () {
-          habilitarBotones();
-          caja.style.backgroundColor = "";
-          texto.innerText = arrayCuestiones[4];
-        }, 3000);
-      } else if (texto.innerText.match("tecnologia")) {
-        if (evento == false) {
-          totalAciertos.innerHTML++;
-          caja.style.backgroundColor = "green";
-        } else {
-          totalFallos.innerHTML++;
-          caja.style.backgroundColor = "red";
-        }
-        vueltas++;
-        desactivarBotones();
-        setTimeout(function () {
-          habilitarBotones();
-          caja.style.backgroundColor = "";
-          totalAciertos.innerHTML = "";
-          totalFallos.innerHTML = "";
-          vueltas = 0;
-          texto.innerText = arrayCuestiones[0];
-          confirm("Se ha acabado el juego");
-        }, 3000);
-      }
-    }
-    trivial();
+function desactivarBotones() {
+  acierto.disabled = true;
+  fallo.disabled = true;
+  acierto.style.backgroundColor = "grey";
+  fallo.style.backgroundColor = "grey";
+}
+
+
+function habilitarBotones() {
+  acierto.disabled = false;
+  fallo.disabled = false;
+  acierto.style.backgroundColor = "green";
+  fallo.style.backgroundColor = "red";
+}
+
+
+function mostrarSiguientePregunta() {
+  if (preguntaActual < arrayPreguntasRespuestas.length) {
+    texto.innerText = arrayPreguntasRespuestas[preguntaActual].pregunta;
+  } else {
+    finDelJuego();
+  }
+}
+
+
+function manejarRespuesta(evento) {
+  const respuestaCorrecta = arrayPreguntasRespuestas[preguntaActual].respuestaCorrecta;
+  const respuestaUsuario = evento;
+
+  if (respuestaCorrecta === respuestaUsuario) {
+    totalAciertos.textContent++;
+    caja.style.backgroundColor = "green";
+  } else {
+    totalFallos.textContent++;
+    caja.style.backgroundColor = "red";
+  }
+
+  preguntaActual++;
+  desactivarBotones();
+
+  setTimeout(() => {
+    habilitarBotones();
+    caja.style.backgroundColor = "";
+    mostrarSiguientePregunta();
+  }, 3000);
+}
+
+function finDelJuego() {
+  totalAciertos.textContent = "";
+  totalFallos.textContent = "";
+  preguntaActual = 0;
+  texto.innerText = "Juego terminado";
+  const reiniciarJuego = confirm("¿Quieres jugar de nuevo?");
+  if (reiniciarJuego) {
+    totalAciertos.textContent = "0";
+    totalFallos.textContent = "0";
+    mostrarSiguientePregunta();
+  }
+}
+
+
+acierto.addEventListener("click", () => manejarRespuesta(true));
+fallo.addEventListener("click", () => manejarRespuesta(false));
+
+mostrarSiguientePregunta();
