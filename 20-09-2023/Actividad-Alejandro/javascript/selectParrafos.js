c1fad031861cf0ca701e11b396b8841ee3d31d4e
ftvalex01 <ftvalex@hotmail.com>
2023-09-24 17:29:46 +0100

    comments

--- 20-09-2023/Actividad-Alejandro/javascript/selectParrafos.js
+++ 20-09-2023/Actividad-Alejandro/javascript/selectParrafos.js
@@ -10,12 +10,14 @@ function selectParrafo() {
   opcionTodos.textContent = "Todos los párrafos";
   selectParrafo.appendChild(opcionTodos);
 
+  //seleccionador de parrafos
   parrafos.forEach((parrafo, i) => {
     let opcionParrafo = document.createElement("option");
     opcionParrafo.value = `parrafo${i}`;
     opcionParrafo.textContent = `Párrafo ${i + 1}`;
     selectParrafo.appendChild(opcionParrafo);
 
+    //funcion para poder seleccionar cada parrafo picando encima de el.
     parrafo.addEventListener("click", () => {
       selectParrafo.value = `parrafo${i}`;
       opcionParrafo.textContent = `Párrafo ${i + 1}`;
