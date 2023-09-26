3e7b3f7d1b054c98121a2f29095823babcf48e74
ftvalex01 <ftvalex@hotmail.com>
2023-09-26 23:15:14 +0100

    1C hecho

--- 20-09-2023/ProyectoCarrito/js/app.js
+++ 20-09-2023/ProyectoCarrito/js/app.js
@@ -69,15 +69,20 @@ function leerDatosCurso(curso) {
      tarjetasCursos.forEach(tarjeta => {
          const autorTarjeta = tarjeta.querySelector('.info-card p').textContent;
          let precioTarjeta = parseFloat(tarjeta.querySelector('.u-pull-right').textContent.replace('$', ''));
- 
+         let descuentoTarjeta = document.createElement('p');
+         descuentoTarjeta.textContent = '!Descuento!';
+         descuentoTarjeta.classList.add('descuento');
+
          if (tarjeta.classList.contains('borde-azul') && precioTarjeta > 10) {
              tarjeta.classList.remove('borde-verde');
              precioTarjeta = Math.max(precioTarjeta - 5, 10);
              tarjeta.querySelector('.u-pull-right').textContent = `$${precioTarjeta}`;
+             tarjeta.appendChild(descuentoTarjeta);
          } else if (autorTarjeta === autorCursoAgregado && precioTarjeta > 10) {
              tarjeta.classList.add('borde-verde');
              precioTarjeta = Math.max(precioTarjeta - 5, 10);
              tarjeta.querySelector('.u-pull-right').textContent = `$${precioTarjeta}`;
+             tarjeta.appendChild(descuentoTarjeta);
          }
          
      });
