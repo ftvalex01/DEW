01f4d38d11005c76829611ae26db982223d7bfd5
ftvalex01 <ftvalex@hotmail.com>
2024-01-10 18:25:37 +0000

    sweetalert puesto

--- actividad-1/src/App.js
+++ actividad-1/src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import './App.css';
 import FormularioPregunta from './components/FormularioPregunta';
 import PreguntasCreadas from './components/PreguntasCreadas';
+import Swal from 'sweetalert2';
 function App() {
 
 const [preguntas, setPreguntas] = useState([]);
@@ -13,7 +14,24 @@ nueva.id = preguntas.length + 1;
 setPreguntas([...preguntas,nueva]);
 }
 const deleteQuestion = (id) => {
-  setPreguntas(preguntas.filter(pregunta => pregunta.id !== id));
+  Swal.fire({
+    title: "BORRAR",
+    text: "deseas borrar la pregunta",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Deleted!",
+        text: "TU PREGUNTA HA SIDO BORRADO",
+        icon: "success"
+      });
+      setPreguntas(preguntas.filter(pregunta => pregunta.id !== id));
+    }
+  });
 };
  
   return (
